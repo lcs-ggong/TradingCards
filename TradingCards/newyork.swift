bafd9b3d9c57f9b61ee7af9f6c28b01beeee5d3c
Grace Gong <ggong@lcs.on.ca>
2023-01-15 17:05:38 -0500

    New York movie done

--- TradingCards/newyork.swift
+++ TradingCards/newyork.swift
@@ -1,5 +1,5 @@
 //
-//  newyork.swift
+//  Newyork.swift
 //  TradingCards
 //
 //  Created by Grace Gong on 2023-01-15.
@@ -7,14 +7,108 @@
 
 import SwiftUI
 
-struct newyork: View {
+struct Newyork: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color("green2.0")
+                .ignoresSafeArea()
+            
+            VStack{
+                
+                ZStack{
+                    
+                    Spacer()
+                    Color("green3.0")
+                    
+                        .frame(width:340.0, height: 450.0)
+                        .cornerRadius(10)
+                    
+                    Image("new")
+                        .frame(width:330.0, height: 450.0)
+                        .cornerRadius(100)
+                    
+                }
+                Spacer()
+                
+                
+                
+                
+                VStack{
+                    Spacer()
+                    ZStack{
+                        LinearGradient(colors: [Color("green1.0"),.white], startPoint: .top , endPoint: .bottomLeading)
+                            .frame(width:390.0, height: 50.0)
+                            .cornerRadius(30)
+                        
+                        HStack{
+                            Spacer()
+                            Text("A Rainy Day in New York")
+                                .font(Font.system(size:30, weight: .bold))
+                                .foregroundColor(Color.gray)
+                            
+                            Spacer()
+                            
+                            
+                        }
+                        .padding(5)}
+                    
+                    
+                    .cornerRadius(40)
+                    
+                }
+            
+                
+               
+                VStack{
+                    Spacer()
+                    
+                    HStack{
+                        
+                        Text("Director: Woody Allen")
+                        
+                            .font(Font.system(size:27, weight: .bold))
+                            .frame(width:330.0, height: 60.0)
+                            .foregroundColor(.gray)
+                        
+                        Spacer()
+                        
+                    }
+                    
+                    
+                    HStack{
+                        Text("Box office: 23.8 million USD")
+                        
+                            .font(Font.system(size:27, weight: .bold))
+                            .frame(width:400.0, height: 5.0)
+                            .foregroundColor(.gray)
+                        Spacer()
+                    }
+                  
+                  
+                    
+                    HStack{
+                        Text("The film follows the romantic exploits of two young college students, Gatsby and Ashleigh (Timothée Chalamet and Elle Fanning), while on a weekend visit to New York City, Gatsby's hometown.")
+                        
+                            .font(Font.system(size:23.5, weight: .regular))
+                            .frame(width:370.0, height: 180.0)
+                            .foregroundColor(.white)
+                        
+                    }
+                  
+                }
+            
+                
+            }
+            
+            
+            
+            
+        }
     }
 }
 
-struct newyork_Previews: PreviewProvider {
+struct Newyork_Previews: PreviewProvider {
     static var previews: some View {
-        newyork()
+        Newyork()
     }
 }
