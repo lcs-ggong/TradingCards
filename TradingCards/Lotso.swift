838bbf89f9aacef8b65c78cde8e502ddc866594d
Grace Gong <ggong@lcs.on.ca>
2023-01-15 02:23:30 -0500

    lotso done

---
+++ TradingCards/Lotso.swift
@@ -0,0 +1,106 @@
+//
+//  Lotso.swift
+//  TradingCards
+//
+//  Created by Grace Gong on 2023-01-15.
+//
+
+import SwiftUI
+
+struct Lotso: View {
+    var body: some View {
+        ZStack{
+            Color("pink1.0")
+                .ignoresSafeArea()
+            
+            
+            VStack{
+                ZStack{
+                    Spacer()
+                    Color("lightyellow2.0")
+                    
+                        .frame(width:340.0, height: 380.0)
+                        .cornerRadius(20)
+                    
+                    Color.white
+                        .frame(width:270.0, height: 340.0)
+                    
+                    Image("lotso")
+                    
+                }
+                Spacer()
+                Spacer()
+                
+                
+                
+                HStack{
+                    
+                    Text("Lotso")
+                        .font(Font.system(size:45, weight: .bold))
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+                }
+                .padding(3)
+                
+                
+                
+                
+                ZStack{
+                    Color("pink2.0")
+                    
+                    
+                        .cornerRadius(40)
+                    
+                    
+                    VStack{
+                        HStack{
+                            
+                            Text("Name: Lotso")
+                            
+                                .font(Font.system(size:25, weight: .bold))
+                                .frame(width:180.0, height: 60.0)
+                                .foregroundColor(.gray)
+                            
+                            Spacer()
+                        }
+                       
+                        HStack{
+                            Text("Played by: Ned Beatty")
+                            
+                                .font(Font.system(size:25, weight: .bold))
+                                .frame(width:290.0, height: 10.0)
+                                .foregroundColor(.gray)
+                            Spacer()
+                        }
+                        Spacer()
+                        Spacer()
+                        
+                        HStack{
+                            Text("Lotso is an old, pink stuffed teddy bear with a strawberry scent and a Southern accent, who uses a wooden toy mallet as a cane (he is still able to walk, even without it). He is first introduced riding over to Woody, Buzz, and the other toys in his dump truck.")
+                            
+                                .font(Font.system(size:22, weight: .regular))
+                                .frame(width:350.0, height: 220.0)
+                                .foregroundColor(.gray)
+                            
+                        }
+                        Spacer()
+                    }
+                
+                }
+            }
+            
+            
+            
+            
+        }
+    }
+    }
+    
+    struct Lotso_Previews: PreviewProvider {
+        static var previews: some View {
+            Lotso()
+        }
+    }
+
